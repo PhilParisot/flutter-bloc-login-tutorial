a0011c29c5a7cd633ecb37bd1b4af8a26bf5bb47
Philippe Parisot <pparisot3@gmail.com>
2019-06-05 10:57:35 -0400

    Tutorial Done

---
+++ lib/bloc/login_bloc.dart
@@ -0,0 +1,40 @@
+import 'dart:async';
+import 'package:bloc/bloc.dart';
+import 'package:flutter_bloc_login_tutorial/userRepository.dart';
+import 'package:meta/meta.dart';
+import './bloc.dart';
+
+class LoginBloc extends Bloc<LoginEvent, LoginState> {
+  final UserRepository userRepository;
+  final AuthenticationBloc authenticationBloc;
+
+  LoginBloc({
+    @required this.userRepository,
+    @required this.authenticationBloc,
+  })  : assert(userRepository != null),
+        assert(authenticationBloc != null);
+
+  @override
+  LoginState get initialState => LoginInitial();
+
+  @override
+  Stream<LoginState> mapEventToState(
+    LoginEvent event,
+  ) async* {
+    if (event is LoginButtonPressed) {
+      yield LoginLoading();
+
+      try {
+        final token = await userRepository.authenticate(
+          username: event.username,
+          password: event.password,
+        );
+
+        authenticationBloc.dispatch(LoggedIn(token: token));
+        yield LoginInitial();
+      } catch (error) {
+        yield LoginFailure(error: error.toString());
+      }
+    }
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,76 @@
+import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_bloc_login_tutorial/homePage.dart';
+import 'package:flutter_bloc_login_tutorial/loadingIndicator.dart';
+import 'package:flutter_bloc_login_tutorial/loginPage.dart';
+import 'package:flutter_bloc_login_tutorial/splashPage.dart';
+import 'package:flutter_bloc_login_tutorial/userRepository.dart';
 
-void main() => runApp(MyApp());
+import 'bloc/bloc.dart';
 
-class MyApp extends StatelessWidget {
+class SimpleBlocDelegate extends BlocDelegate {
+  @override
+  void onTransition(Bloc bloc, Transition transition) {
+    super.onTransition(bloc, transition);
+    print(transition);
+  }
+}
+
+void main() {
+  BlocSupervisor.delegate = SimpleBlocDelegate();
+  runApp(App(userRepository: UserRepository()));
+}
+
+class App extends StatefulWidget {
+  final UserRepository userRepository;
+
+  App({Key key, @required this.userRepository}) : super(key: key);
+
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  AuthenticationBloc authenticationBloc;
+  UserRepository get userRepository => widget.userRepository;
+
+  @override
+  void initState() {
+    authenticationBloc = AuthenticationBloc(userRepository: userRepository);
+    authenticationBloc.dispatch(AppStarted());
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    authenticationBloc.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return BlocProvider<AuthenticationBloc>(
+      bloc: authenticationBloc,
+      child: MaterialApp(
+        home: BlocBuilder<AuthenticationEvent, AuthenticationState>(
+          bloc: authenticationBloc,
+          builder: (BuildContext context, AuthenticationState state) {
+            if (state is AuthenticationUninitialized) {
+              return SplashPage();
+            }
+            if (state is AuthenticationAuthenticated) {
+              return HomePage();
+            }
+            if (state is AuthenticationUnauthenticated) {
+              return LoginPage(userRepository: userRepository);
+            }
+            if (state is AuthenticationLoading) {
+              return LoadingIndicator();
+            }
+          },
+        ),
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
